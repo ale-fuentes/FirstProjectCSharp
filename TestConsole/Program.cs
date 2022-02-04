95cf6ddde55097c4222d8e6015a9371fe151ed61
Raul Alejandro Fuentes <raul.a.fuentes@avanade.com>
2022-02-04 18:21:49 -0300

    doc - starting module linguagem de programacao com C#

--- TestConsole/Program.cs
+++ TestConsole/Program.cs
@@ -1,4 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
+// by alefuentes
 
 var myText = "Testing";
+int age;            // is correct, init with default value, in int are ZERO
+int otherAge = 25;  // is correct, init with 25
+var sameAge = 25;   // is correct, init with 25
+//var againAge;       // Erro, var need initialized
+
+
+const int MIN_AGE = 20;
+/*
+const int MAX_AGE; // Erro, var need initialized
+const var MAX_AGE = 20 // Erro, cannot use var with const 
+*/
+
 Console.WriteLine("Hello, World!, " + myText);
+
+
