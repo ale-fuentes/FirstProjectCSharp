2ad1b87b814304b908ab2af0922ded08a5f56c52
Raul Alejandro Fuentes <raul.a.fuentes@avanade.com>
2022-03-30 11:35:46 -0300

    fat: add new doc and project

--- TestConsole/Program.cs
+++ TestConsole/Program.cs
@@ -3,8 +3,8 @@
 
 var myText = "Testing";
 int age;            // is correct, init with default value, in int are ZERO
-int otherAge = 25;  // is correct, init with 25
-var sameAge = 25;   // is correct, init with 25
+// int otherAge = 25;  // is correct, init with 25
+// var sameAge = 25;   // is correct, init with 25
 //var againAge;       // Erro, var need initialized
 
 
@@ -14,6 +14,30 @@ const int MAX_AGE; // Erro, var need initialized
 const var MAX_AGE = 20 // Erro, cannot use var with const 
 */
 
-Console.WriteLine("Hello, World!, " + myText);
+byte myByte = 127;
+sbyte MySbyte = -128;
 
+int myInt = 128;
+uint myUint = 152625;
+long myLong = 325;
 
+double myDouble = 128.0005;
+float myFloat = 128.0005f;
+decimal myDecimal = 128.0005m;
+
+bool myBool = true;
+bool myBoolFalse = false;
+
+char myCharUnicode = 'a';
+
+string myString = "Hi multiverse";
+
+object myObject = "This are a object";
+Console.WriteLine("Hello, World!, " + myObject);
+myObject = 125;
+Console.WriteLine("Hello, World!, " + myObject);
+
+int? myIntNullable = null;
+Console.WriteLine("Hello, World!, " + myIntNullable);
+myIntNullable = 25;
+Console.WriteLine("Hello, World!, " + myIntNullable);
