352f568f9fa74910d3ce74f13ef0c7e725222deb
Raul Alejandro Fuentes <raul.a.fuentes@avanade.com>
2022-03-30 12:12:22 -0300

    feat - add project calculator

---
+++ Calculator/Program.cs
@@ -0,0 +1,99 @@
+﻿// See https://aka.ms/new-console-template for more information
+Menu();
+
+
+static void Menu()
+{
+    Console.Clear();
+    Console.WriteLine("Hello, World!\n");
+    Console.WriteLine("1- Addition");
+    Console.WriteLine("2- Substration");
+    Console.WriteLine("3- Multiplication");
+    Console.WriteLine("4- Division");
+    Console.WriteLine("5- Exit");
+    Console.WriteLine("\nSelect your options:");
+    short option = short.Parse(Console.ReadLine());
+
+    switch (option)
+    {
+        case 1: Addition(); break;
+        case 2: Substration(); break;
+        case 3: Multiplication(); break;
+        case 4: Division(); break;
+        case 5: System.Environment.Exit(0); break;
+        default: Menu(); break;
+    }
+}
+
+static void Addition()
+{
+    Console.Clear();
+    Console.WriteLine("Hello, World!");
+
+    Console.WriteLine("First value: ");
+    float v1 = float.Parse(Console.ReadLine());
+
+    Console.WriteLine("Second value: ");
+    float v2 = float.Parse(Console.ReadLine());
+
+    float result = v1 + v2;
+    Console.WriteLine($"\nAddition between this two values is -> {result}");
+    Console.ReadKey();
+    Menu();
+}
+
+static void Substration()
+{
+    Console.Clear();
+    Console.WriteLine("Hello, World!");
+
+    Console.WriteLine("First value: ");
+    float v1 = float.Parse(Console.ReadLine());
+
+    Console.WriteLine("Second value: ");
+    float v2 = float.Parse(Console.ReadLine());
+
+    float result = v1 - v2;
+    Console.WriteLine($"\nSubstration between this two values is -> {result}");
+    Console.ReadKey();
+    Menu();
+}
+
+static void Multiplication()
+{
+    Console.Clear();
+    Console.WriteLine("Hello, World!");
+
+    Console.WriteLine("First value: ");
+    float v1 = float.Parse(Console.ReadLine());
+
+    Console.WriteLine("Second value: ");
+    float v2 = float.Parse(Console.ReadLine());
+
+    float result = v1 * v2;
+    Console.WriteLine($"\nMultiplication between this two values is -> {result}");
+    Console.ReadKey();
+    Menu();
+}
+
+static void Division()
+{
+    Console.Clear();
+    Console.WriteLine("Hello, World!");
+
+    Console.WriteLine("First value: ");
+    float v1 = float.Parse(Console.ReadLine());
+
+    float v2;
+    do
+    {
+        Console.WriteLine("Second value (different to zero): ");
+        v2 = float.Parse(Console.ReadLine());
+    } while (v2 == 0);
+
+
+    float result = v1 / v2;
+    Console.WriteLine($"\nDivision between this two values is -> {result}");
+    Console.ReadKey();
+    Menu();
+}
---
+++ Calculator/README.md
@@ -0,0 +1,17 @@
+# CLI :: Command Line Interface
+![](https://img.shields.io/badge/Code-C_Sharp-informational?style=flat&logo=csharp&logoColor=white&color=007acc)
+
+[Return HOME](README.md)
+
+
+#### Example of show string with Writeline
+> float v1 = float.Parse(Console.ReadLine());
+> float v2 = float.Parse(Console.ReadLine());
+> float result = v1 + v2;
+>
+> Console.WriteLine($"\nSum two values is -> " + result);
+> Console.WriteLine($"\nSum two values is -> " + (v1 + v2)); 
+> Console.WriteLine($"\nSum two values is -> " + v1 + v2); // mergue string
+> Console.WriteLine($"\nSum two values is -> {v1 + v2}");
+> Console.WriteLine($"\nSum two values is -> {result}");
+
