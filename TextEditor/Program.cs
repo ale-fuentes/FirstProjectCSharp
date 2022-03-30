35f1c3a239b0b340ed2364a85329091f130d7793
Raul Alejandro Fuentes <raul.a.fuentes@avanade.com>
2022-03-30 13:12:07 -0300

    feat - add new project texteditor

---
+++ TextEditor/Program.cs
@@ -0,0 +1,71 @@
+﻿// See https://aka.ms/new-console-template for more information
+Menu();
+
+static void Menu()
+{
+    Console.Clear();
+    Console.WriteLine("Hello, World!\nYour options:");
+    Console.WriteLine("1- Open file");
+    Console.WriteLine("2- Crate new file");
+    Console.WriteLine("3- Exit");
+    Console.WriteLine("\nSelect your options:");
+    short option = short.Parse(Console.ReadLine());
+
+    switch (option)
+    {
+        case 1: Open(); break;
+        case 2: Edit(); break;
+        case 3: System.Environment.Exit(0); break;
+        default: Menu(); break;
+    }
+}
+
+static void Open()
+{
+    Console.Clear();
+    Console.WriteLine("Digit please the path and name to file: ");
+    var path = Console.ReadLine();
+
+    using (var file = new StreamReader(path))
+    {
+        string text = file.ReadToEnd();
+        Console.Write(text);
+    }
+
+    Console.WriteLine("\n >> File reading with success!");
+    Console.ReadKey();
+    Menu();
+}
+
+static void Edit()
+{
+    Console.Clear();
+    Console.WriteLine("Digit please the name to file (ESC for exit): ");
+    String text = String.Empty;
+
+    do
+    {
+        text += Console.ReadLine();
+        text += Environment.NewLine;
+    } while (Console.ReadKey().Key != ConsoleKey.Escape);
+
+    Save(text);
+
+
+}
+
+static void Save(string text)
+{
+    Console.Clear();
+    Console.WriteLine("What name of file? :");
+    var path = Console.ReadLine();
+
+    using (var file = new StreamWriter(path))
+    {
+        file.Write(text);
+    }
+    Console.WriteLine("\n >> File save with success!");
+    Console.ReadKey();
+    Menu();
+
+}
---
+++ TextEditor/README.md
@@ -0,0 +1,7 @@
+# Project :: Text Editor
+![](https://img.shields.io/badge/Code-C_Sharp-informational?style=flat&logo=csharp&logoColor=white&color=007acc)
+
+[Return HOME](README.md)
+
+
+
